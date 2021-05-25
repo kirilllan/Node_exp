4873ee84092f876e9d3203d97dd6c30e4ac79e3e
kirilllan <s2100138@edu.bc.fi>
2021-05-25 08:42:11 +0300

    EvntEmmitr & fileCrt

--- p1/app.js
+++ p1/app.js
@@ -1,18 +1,3 @@
 ;console.log('From p1 app.js')
 ;
 
-const getText = path => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-  })
-  })
-}
-
-getText('.content/first.txt')
-  .then((result) => console.log(result))
-  .catch(err=>console.lof(err))
--- p1/bigfile.js
+++ p1/bigfile.js
@@ -40,7 +40,7 @@ readFile('./content/first.txt', 'utf8', (err, res) => {
 console.log('starting next task') */
 
 
-// --12-HTTP
+// ----12-HTTP
 /* const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -54,3 +54,52 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(5000) */
+
+
+// ---- file manipulation
+// creates a file with content + content from other files
+// "'THIS IS AWSUM' Halo dis iz firzt tex file second txt file in content/secont.txt"
+/* const {readFile, writeFile} = require('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+const start = async() => {
+  try {
+    const first = await readFilePromise('./content/first.txt')
+    const second = await readFilePromise('./content/second.txt')
+    await writeFilePromise('./content/result-mind-fruit.txt', `'THIS IS AWSUM' ${first} ${second}`)
+    console.log(first, second)
+  } catch (err) {
+    console.log(err)
+  }
+}
+start() */
+
+/* const getText = path => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+  })
+  })
+}
+
+getText('./content/first.txt')
+  .then((result) => console.log(result))
+  .catch(err=>console.log(err)) */
+
+
+// ---- EventEmitter
+/* const EventEmitter = require('events')
+const customEmitter = new EventEmitter()
+customEmitter.on('response', (name, id) => {
+  console.log(`data received user ${name} with id:${id} `);
+})
+customEmitter.on('response', () => {
+  console.log(`some other logic.. `);
+})
+customEmitter.emit('response', 'john', 44) */
