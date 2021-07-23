9a29717c13b47c9d4461b40d1a8951ddd5e36540
kirilllan <s2100138@edu.bc.fi>
2021-07-23 15:34:00 +0300

    icon&title

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 //constant, no $ sign anywhere
 define('PI', 3.1415);
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">
-  <title>Document</title>
+  <link rel="icon" type="text" href="P.png">
+  <title>PHP sheet</title>
 </head>
 <body>
+  <p>PI: <?php echo PI ?></p>
 </body>
 </html>
