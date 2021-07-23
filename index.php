0370a6b5bb5a3d33cd81c118196e463f20e2f9b5
kirilllan <s2100138@edu.bc.fi>
2021-07-23 16:08:33 +0300

    str manipulations

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ define('PI', 3.1415);
 //ceil(),floor(),round(),max(),min(),pow(),sqrt(),pi(),mt_rand()//faster random number
 //is_finite(),is_numeric(),number_format()==truncate to x decimals.//also geom. fns. 
 
+// STRINGS
 printf("%c %d %.2f %s<br>", 65, 65.5, 65.543, 65);//data type change -> c=charCode,d=int,2 decimal place float, str
+//strlen(),ltrim(),rtrim(),strtoupper(),strtolower(),ucfirst(),substr($,0,1),strpos('ab'),str_replace('yes','no',$)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,17 +16,10 @@ printf("%c %d %.2f %s<br>", 65, 65.5, 65.543, 65);//data type change -> c=charCo
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="text" href="P.png">
+  <link rel="shortcut icon" href="P.png" type="image/x-icon">
   <title>PHP sheet</title>
 </head>
 <body>
-  <p>PI: <?php echo PI ?></p>
-  <form action="index.php" method="get">
-    <label for="">Your State: </label>
-    <input type="text" name="state"><br>
-    <label for="">Number 1: </label>
-    <input type="text" name="num-1"><br>
-    <input type="text">
-  </form>
+
 </body>
 </html>
