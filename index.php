8df06bf4fd7c7d64de9f57db45bfa9e665e6140e
kirilllan <s2100138@edu.bc.fi>
2021-07-27 11:21:15 +0300

    style file

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ try {badDivide(0);} catch(Exception $e) {echo $e->getMessage();}
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="P.png" type="image/x-icon">
+  <link rel="stylesheet" href="main.css">
   <title>PHP sheet</title>
 </head>
 <body>
